ba23afedd7fbd2095cc1cbd4e1fe9fdc2ceb4857
Ilya Pugachev <ilyapugachv@gmail.com>
2024-07-22 21:32:39 +0300

    InfoBar about film completed

--- CineSeeker/CineSeeker/Modules/Home/Detail/Detail+View.swift
+++ CineSeeker/CineSeeker/Modules/Home/Detail/Detail+View.swift
@@ -6,6 +6,7 @@ extension Detail {
         // MARK: - Properties -
         
         var presenter: Presenter!
+        private lazy var safeArea = self.view.safeAreaLayoutGuide
         
         // MARK: - Subviews -
         
@@ -16,8 +17,29 @@ extension Detail {
         private let star: UIImageView = .init()
         private let ratingLabel: UILabel = .init()
         
-        // MARK: - Initializers -
+        private let calendarStackView: UIStackView = .init()
         
+        private let calendarImageView: UIImageView = .init()
+        private let yearFilmLabel: UILabel = .init()
+        
+        private let stripeOne: UIView = .init()
+        
+        private let watchStackView: UIStackView = .init()
+        
+        private let watchImageView: UIImageView = .init()
+        private let durationFilmLabel: UILabel = .init()
+        
+        private let stripeTwo: UIView = .init()
+        
+        private let actionStackView: UIStackView = .init()
+        
+        private let actionImageView: UIImageView = .init()
+        private let actionFilmLabel: UILabel = .init()
+        
+        private let mainStackView: UIStackView = .init()
+        
+        // MARK: - Initializers -
+    
         public init(with presenter: Presenter) {
             self.presenter = presenter
             super.init(nibName: nil, bundle: nil)
@@ -57,6 +79,23 @@ extension Detail {
             posterFilmImageView.addView(blurView)
             blurView.contentView.addView(star)
             blurView.contentView.addView(ratingLabel)
+            
+            calendarStackView.addArrangedSubview(calendarImageView)
+            calendarStackView.addArrangedSubview(yearFilmLabel)
+            
+            watchStackView.addArrangedSubview(watchImageView)
+            watchStackView.addArrangedSubview(durationFilmLabel)
+            
+            actionStackView.addArrangedSubview(actionImageView)
+            actionStackView.addArrangedSubview(actionFilmLabel)
+            
+            mainStackView.addArrangedSubview(calendarStackView)
+            mainStackView.addArrangedSubview(stripeOne)
+            mainStackView.addArrangedSubview(watchStackView)
+            mainStackView.addArrangedSubview(stripeTwo)
+            mainStackView.addArrangedSubview(actionStackView)
+            
+            view.addView(mainStackView)
         }
         
         private func configureSubviews() {
@@ -86,10 +125,61 @@ extension Detail {
             star.image = UIImage(systemName: "star")
             star.tintColor = .Colors.orange
             star.contentMode = .scaleAspectFit
+            
+            calendarImageView.image = UIImage(systemName: "calendar")
+            calendarImageView.tintColor = .white
+            
+            yearFilmLabel.configureLabel(
+                text: "2021",
+                font: .interMedium(of: 12),
+                color: .white
+            )
+            
+            stripeOne.backgroundColor = .white
+            stripeOne.widthAnchor.constraint(equalToConstant: 1).isActive = true
+            stripeOne.heightAnchor.constraint(equalToConstant: 16).isActive = true
+            
+            calendarStackView.axis = .horizontal
+            calendarStackView.spacing = 5
+            calendarStackView.alignment = .center
+            
+            watchImageView.image = UIImage(systemName: "stopwatch")
+            watchImageView.tintColor = .white
+            
+            durationFilmLabel.configureLabel(
+                text: "148 Minutes",
+                font: .interMedium(of: 12),
+                color: .white
+            )
+            
+            stripeTwo.backgroundColor = .white
+            stripeTwo.widthAnchor.constraint(equalToConstant: 1).isActive = true
+            stripeTwo.heightAnchor.constraint(equalToConstant: 16).isActive = true
+            
+            watchStackView.axis = .horizontal
+            watchStackView.spacing = 5
+            watchStackView.alignment = .center
+            
+            actionImageView.image = UIImage(systemName: "seal")
+            actionImageView.tintColor = .white
+            
+            actionFilmLabel.configureLabel(
+                text: "Action",
+                font: .interMedium(of: 12),
+                color: .white
+            )
+            
+            actionStackView.axis = .horizontal
+            actionStackView.spacing = 5
+            actionStackView.alignment = .center
+            
+            mainStackView.axis = .horizontal
+            mainStackView.spacing = 10
+            mainStackView.alignment = .center
+            mainStackView.distribution = .equalSpacing
         }
         
         private func layoutSubviews() {
-            
             
             NSLayoutConstraint.activate([
                 posterFilmImageView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -114,6 +204,11 @@ extension Detail {
                 nameMovieLabel.topAnchor.constraint(equalTo: posterFilmImageView.bottomAnchor, constant: 20),
                 nameMovieLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
                 nameMovieLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+                
+                mainStackView.topAnchor.constraint(equalTo: nameMovieLabel.bottomAnchor, constant: 20),
+                mainStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 12),
+                mainStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -12),
+                mainStackView.heightAnchor.constraint(equalToConstant: 32)
             ])
         }
         
